38baa9a3f16ffe11006b666fe267725f95a51403
Jodecir Neto <jodecirneto@gmail.com>
2020-12-07 10:22:09 -0300

    wasd keys

--- js/script.js
+++ js/script.js
@@ -63,6 +63,11 @@ function update (event){
 	if (event.keyCode == 38 && direction != "down") direction = "up";
 	if (event.keyCode == 39 && direction != "left") direction = "right";
 	if (event.keyCode == 40 && direction != "up") direction = "down";
+
+	if (event.keyCode == 65 && direction != "right") direction = "left";
+	if (event.keyCode == 87 && direction != "down") direction = "up";
+	if (event.keyCode == 68 && direction != "left") direction = "right";
+	if (event.keyCode == 83 && direction != "up") direction = "down";
 }
 
 function startGame(){
@@ -120,7 +125,7 @@ function startGame(){
 			snake.pop();
 			tailSize--;
 			document.getElementById("tailSize").innerHTML = "Metros da Pitón: " + tailSize;
-			par = par - 2;
+			par = par - 1;
 		}
 	}
 	else {
