6cff174b67005109cbbb03ae2523cc55fd6a4eb9
Jodecir Neto <jodecirneto@gmail.com>
2020-12-12 16:34:53 -0300

    dark mode update

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,5 +1,6 @@
+const html = document.querySelector('html');
 const chk = document.getElementById('chk');
 
-chk.addEventListener('change', () => {
-	document.body.classList.toggle('dark');
+chk.addEventListener('change', function() {
+	html.classList.toggle('dark');
 });
