fe36aa27ed4251a9fd0d55ca7218b55152b45553
Jodecir Neto <jodecirneto@gmail.com>
2021-07-21 19:19:31 -0300

    UPDATE spacing in code

--- js/script.js
+++ js/script.js
@@ -30,36 +30,36 @@ let banana = {
 	y: Math.floor(Math.random() * 15 + 1) * box
 }
 
-function backgroundCreate(){
+function backgroundCreate() {
 	context.fillStyle = "lightgreen";
 	context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function createHead(){
+function createHead() {
 	for (i=0; i < snake.length; i++) {
 		context.fillStyle = "green";
 		context.fillRect(snake[i].x, snake[i].y, box, box);
 	}
 }
 
-function drawApple(){
+function drawApple() {
 	context.fillStyle = "red";
 	context.fillRect(apple.x, apple.y, box, box);
 }
 
-function drawOrange(){
+function drawOrange() {
 	context.fillStyle = "orange";
 	context.fillRect(orange.x, orange.y, box, box);
 }
 
-function drawBanana(){
+function drawBanana() {
 	context.fillStyle = "yellow";
 	context.fillRect(banana.x, banana.y, box, box);
 }
 
 document.addEventListener('keydown', update);
 
-function update (event){
+function update (event) {
 	if (event.keyCode == 37 && direction != "right") direction = "left";
 	if (event.keyCode == 38 && direction != "down") direction = "up";
 	if (event.keyCode == 39 && direction != "left") direction = "right";
@@ -71,20 +71,20 @@ function update (event){
 	if (event.keyCode == 83 && direction != "up") direction = "down";
 }
 
-function startGame(){
+function startGame() {
 	if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
 	if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
 	if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
 	if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
-	for(i=1; i < snake.length; i++){
+	for(i=1; i < snake.length; i++) {
 		if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
 			clearInterval(jogo);
 			gameOver();    
 		}
 	}
 
-	for(i=0; i < snake.length; i++){
+	for(i=0; i < snake.length; i++) {
 		if (tailSize <= 0){
 			clearInterval(jogo);
 			gameOverOrange();  
@@ -105,10 +105,10 @@ function startGame(){
 	if(direction == "up") snakeY -= box;
 	if(direction == "down") snakeY += box;
 
-	if(snakeX != apple.x || snakeY != apple.y){
+	if(snakeX != apple.x || snakeY != apple.y) {
 			snake.pop();
-		if(snakeX != orange.x || snakeY != orange.y){			
-			if(snakeX != banana.x || snakeY != banana.y){	
+		if(snakeX != orange.x || snakeY != orange.y) {			
+			if(snakeX != banana.x || snakeY != banana.y) {	
 			} else {
 				banana.x = Math.floor(Math.random() * 15 + 1) * box;
 				banana.y = Math.floor(Math.random() * 15 + 1) * box;
@@ -162,6 +162,5 @@ let gameOver = () => {
 let gameOverOrange = () => {
 	alert('Laranjas fazem sua cobrinha ficar menor , você chegou no seu limite e morreu de desnutrição X_X');        
 }
-
 
 let jogo = setInterval(startGame, 100);
